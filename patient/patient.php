38808a8c4fd1a124ef578ab380678f487318f8e0
IK Mendoza <iken.mndz@gmail.com>
2023-09-24 10:33:32 +0800

    fix tab header icon

--- patient/patient.php
+++ patient/patient.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://eluvohealth.com/wp-content/uploads/2023/07/favicon-150x150.png" sizes="32x32">
     <link rel="stylesheet" href="../css/animations.css">  
     <link rel="stylesheet" href="../css/main.css">  
     <link rel="stylesheet" href="../css/admin.css">
