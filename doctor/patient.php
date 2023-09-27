7c610c822b273af52054669652d782133ad9ad8f
IK Mendoza <iken.mndz@gmail.com>
2023-09-27 19:25:50 +0800

    patient NIC

--- doctor/patient.php
+++ doctor/patient.php
@@ -242,12 +242,6 @@
                                 
                                 </th>
                                 <th class="table-headin">
-                                    
-                                
-                                    NIC
-                                    
-                                </th>
-                                <th class="table-headin">
                                 
                             
                                 Telephone
@@ -297,7 +291,6 @@
                                     $pid=$row["pid"];
                                     $name=$row["pname"];
                                     $email=$row["pemail"];
-                                    $nic=$row["pnic"];
                                     $dob=$row["pdob"];
                                     $tel=$row["ptel"];
                                     
@@ -305,9 +298,6 @@
                                         <td> &nbsp;'.
                                         substr($name,0,35)
                                         .'</td>
-                                        <td>
-                                        '.substr($nic,0,12).'
-                                        </td>
                                         <td>
                                             '.substr($tel,0,10).'
                                         </td>
@@ -354,7 +344,6 @@
             $row=$result->fetch_assoc();
             $name=$row["pname"];
             $email=$row["pemail"];
-            $nic=$row["pnic"];
             $dob=$row["pdob"];
             $tele=$row["ptel"];
             $address=$row["paddress"];
@@ -409,16 +398,7 @@
                                 '.$email.'<br><br>
                                 </td>
                             </tr>
-                            <tr>
-                                <td class="label-td" colspan="2">
-                                    <label for="nic" class="form-label">NIC: </label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="label-td" colspan="2">
-                                '.$nic.'<br><br>
-                                </td>
-                            </tr>
+                            
                             <tr>
                                 <td class="label-td" colspan="2">
                                     <label for="Tele" class="form-label">Telephone: </label>
