8f6c8712fbf9a5e81867310266a3c3d9a6b754e3
IK Mendoza <iken.mndz@gmail.com>
2023-10-05 17:17:57 +0800

    master connection

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $database= new mysqli("localhost","root","","edoc");
+    $database= new mysqli("localhost","u337390497_eluvoedoc","codEovulE23!","u337390497_edoc");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
