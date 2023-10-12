89c3ab951f4e05a5911fb30bd16c9fb35a65c2cc
IK Mendoza <iken.mndz@gmail.com>
2023-10-12 21:01:48 +0800

    .gitignore is now working

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $database= new mysqli("localhost","u337390497_eluvoedoc","codEovulE23!","u337390497_edoc");
-    if ($database->connect_error){
-        die("Connection failed:  ".$database->connect_error);
-    }
-
-?>
